a51499038d544bd8134d733bdbeaec596e313fe9
Liam <liamn7101996@gmail.com>
2024-07-12 14:25:24 +0300

    changed doc

--- malloc_calloc.c
+++ malloc_calloc.c
@@ -7,13 +7,13 @@ int main(){
     int i;
     scanf("%d",&count);
     arr = malloc(count * sizeof(*arr));
-    //malloc sets the bytes for the array in the memmory, 
-    //calloc does the same only it sets the bytes as 0's before the array is set 
+    /*malloc sets the bytes for the array in the memmory, 
+    calloc does the same only it sets the bytes as 0's before the array is set */
     for (i=0;i<count;i++)
     {
         scanf("%d",&arr[i]);
     }
     free(arr);
-    //free the memory used in the array, that way we dont have a memory leak
+    /*free the memory used in the array, that way we dont have a memory leak*/
     return 0;
 }
